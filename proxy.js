df83f66f54dff88173c51bd29f02c457a36a63f6
Matthew Dobson <mdobson4@gmail.com>
2015-03-20 14:44:36 -0400

    Updates to tests to move proxy connection tests to another file, and proxy API test additions.

--- proxy.js
+++ proxy.js
@@ -45,8 +45,9 @@ Proxy.prototype._setup = function() {
   });
 
   this._server.on('request', function(request, response) {
+    console.log('request event');
     var parsed = url.parse(request.url, true);
-
+    
     if (parsed.path === '/') {
       self._serveRoot(request, response)
     } else {
