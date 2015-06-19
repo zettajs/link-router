3632a167973faf77f70c0114b1e298f6562e95b6
Adam Magaluk <AdamMagaluK@gmail.com>
2015-06-19 16:28:12 -0400

    Basic working monitor service

--- monitor/service.js
+++ monitor/service.js
@@ -1,10 +1,13 @@
-var TargetState = require('./target-state');
+var async = require('async');
+var TargetState = require('./target_state');
+var targetCheck = require('./target_check');
 
 var Defaults = {
-  Interval: 30,
-  Timeout: 10,
+  Interval: 30000,
+  Timeout: 10000,
   HealthyTreshold: 2,
-  UnhealthyThreshold: 5
+  UnhealthyThreshold: 5,
+  MaxParrell: 5
 };
 
 var MonitorService = module.exports = function(serviceRegistryClient, opts) {
@@ -14,11 +17,6 @@ var MonitorService = module.exports = function(serviceRegistryClient, opts) {
     opts = {};
   }
 
-  // If disabled don't do anything
-  if (opts.disabled === true) {
-    return;
-  }
-  
   if (!serviceRegistryClient) {
     throw new Error('ServiceRegistryClient must be supplied');
   }
@@ -31,11 +29,76 @@ var MonitorService = module.exports = function(serviceRegistryClient, opts) {
     }
   });
 
+  if (!this.log) {
+    this.log = console;
+  }
+
   this.state = {}; // { <targetUrl>: TargetState }
+
+  // If disabled don't do anything
+  if (opts.disabled === true) {
+    return;
+  } else {
+    this.start();
+  }
 };
 
-MonitorService.prototype.getherHosts = function(callback) {
-  
-}; 
+MonitorService.prototype.start = function() {
+  clearInterval(this._intervalTimer);
+  this._intervalTimer = setInterval(this._run.bind(this), this.Interval);
+  this._run();
+};
+
+MonitorService.prototype.stop = function() {
+  clearInterval(this._intervalTimer);
+};
+
+MonitorService.prototype.status = function(targetUrl) {
+  if (!this.state.hasOwnProperty(targetUrl)) {
+    return 'UNDETERMINED';
+  } else {
+    return this.state[targetUrl].status;
+  }
+};
+
+MonitorService.prototype._run = function() {
+  var self = this;
+  // gather hosts
+  // foreach host check and update state
+
+  this.getherHosts(function(err, hosts) {
+    if (err) {
+      self.log.error('Monitor: Failed to gather targets. ' + err);
+      return;
+    }
+    
+    async.eachLimit(hosts, self.MaxParrell, self._updateHost.bind(self), function(err) {});
+  });
+};
+
+// check and update host state
+MonitorService.prototype._updateHost = function(target, callback) {
+  if (!this.state.hasOwnProperty(target.url)) {
+    this.state[target.url] = new TargetState(this.HealthyThreshold, this.UnHealthyThreshold);
+  }
+
+  var state = this.state[target.url];
+
+  var opts = {
+    Timeout: this.Timeout
+  };
+  targetCheck(opts, target, function(result, err) {
+    if (result) {
+      state.success();
+    } else {
+      state.fail();
+    }
+    callback();
+  });
+};
+
+MonitorService.prototype.gatherHosts = function(callback) {
+  this.serviceRegistryClient.findAll(callback);
+};
 
 MonitorService.Defaults = Defaults;
--- test/test_target_monitor.js
+++ test/test_target_monitor.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var MockEtcd = require('./mocks/mock_etcd');
 var ServiceRegistryClient = require('../service_registry_client');
 var MonitorService = require('../monitor/service');
+var TargetState = require('../monitor/target_state');
 
 describe('Target Monitor', function() {
   var etcd = null;
@@ -25,17 +26,89 @@ describe('Target Monitor', function() {
   describe('Constructor Options', function() {
 
     it('without options defaults will be used', function() {
-      var monitor = new MonitorService(serviceRegistryClient);
+      var monitor = new MonitorService(serviceRegistryClient, { disabled: true });
       Object.keys(MonitorService.Defaults).forEach(function(k) {
         assert.equal(monitor[k], MonitorService.Defaults[k]);
       });
     });
 
     it('allow overide of options', function() {
-      var monitor = new MonitorService(serviceRegistryClient, { Interval: 55 });
-      assert.equal(monitor.Interval, 55);
+      var monitor = new MonitorService(serviceRegistryClient, { disabled: true, Interval: 60000 });
+      assert.equal(monitor.Interval, 60000);
+    });
+
+    it('gatherHosts return all etcd hosts', function(done) {
+      etcd.set('/services/zetta/foo', '{"url":"http://example.com/", "tenantId": "default", "version": "1"}');
+      etcd.set('/services/zetta/bar', '{"url":"http://hello.com/", "tenantId": "default", "version": "2"}');
+      var monitor = new MonitorService(serviceRegistryClient, { disabled: true });
+      monitor.gatherHosts(function(err, hosts) {
+        assert(!err);
+        assert.equal(hosts.length, 2);
+        done();
+      });
+    });
+
+    it('_updateTarget should set state', function(done) {
+      var target = { url: 'http://localhost:1', version: "1" };
+      var monitor = new MonitorService(serviceRegistryClient, { disabled: true });
+      monitor._updateHost(target, function() {
+        assert.equal(monitor.status(target.url), 'DOWN')
+        done();
+      });
     });
 
   });
+
+  describe('Target State', function() {
+
+    it('Starts in undetermined state', function() {
+      var state = new TargetState();
+      assert.equal(state.status, 'UNDETERMINED');
+    })
+
+    it('Constructor takes healthy/unhealthy thresholds', function() {
+      var state = new TargetState(5,6);
+      assert.equal(state.HealthyThreshold, 5);
+      assert.equal(state.UnhealthyThreshold, 6);
+    })
+
+    it('success change state to UP if started in undetermined state', function() {
+      var state = new TargetState(2, 2);
+      state.success();
+      assert.equal(state.status, 'UP');
+      state.success();
+      assert.equal(state.status, 'UP');
+    })
+
+    it('fail change state to DOWN if started in undetermined state', function() {
+      var state = new TargetState(2, 2);
+      state.fail();
+      assert.equal(state.status, 'DOWN');
+      state.fail();
+      assert.equal(state.status, 'DOWN');
+    })
+
+    it('from DOWN state one success should still keep it in the DOWN state', function() {
+      var state = new TargetState(2, 2);
+      state.fail();
+      state.fail();
+      assert.equal(state.status, 'DOWN');
+      state.success();
+      assert.equal(state.status, 'DOWN');
+      state.success();
+      assert.equal(state.status, 'UP');
+    })
+
+    it('from UP state one success should still keep it in the UP state', function() {
+      var state = new TargetState(2, 2);
+      state.success();
+      state.success();
+      assert.equal(state.status, 'UP');
+      state.fail();
+      assert.equal(state.status, 'UP');
+      state.fail();
+      assert.equal(state.status, 'DOWN');
+    })
+  })
 
 });
